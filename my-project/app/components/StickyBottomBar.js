975a3627c95cfff879e577ecf74e943a81e4bc46
DoppalapudiSravani <dsravani970@gmail.com>
2024-02-02 11:40:26 +0530

    sticky bar

---
+++ my-project/app/components/StickyBottomBar.js
@@ -0,0 +1,80 @@
+import React from 'react'
+
+const StickyBottomBar = () => {
+  return (
+    <>
+    <div className='fixed bottom-0 left-0 w-full h-[17%] bg-gray-100 p-4  z-10 border-soild boarder-6 mr-20'>
+      
+        <div className='flex justify-center w-[100%] h-[40%] '>
+         
+        <img className='w[5%] h-[4rem] mr-4 rounded'src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRjq5pKbroM7jcWOjJr9JT-AY5IuZGB-3YNcA&usqp=CAU'></img>
+        <div className='text-black text-sm'>fresho
+        <p className='text-black mr-5'>fresh Capsicum-green (Loosa),1 kg...</p></div>
+
+        <div>
+        <div className=' flex text-sm text-black  '>MRP
+        
+        <img className=' text-black h-[2%] w-[4%] rounded 'src='https://t4.ftcdn.net/jpg/01/88/66/97/240_F_188669798_TzP74MjnqVivXdnJeVntMlF7fnASfHAy.jpg' ></img>
+        <p className='text-black '>98</p></div>
+         <div>    
+        <div className='flex'>
+          
+        <img className='text-black w-[5%] h-[4%]'src='https://t4.ftcdn.net/jpg/01/88/66/97/240_F_188669798_TzP74MjnqVivXdnJeVntMlF7fnASfHAy.jpg' ></img>98(
+        <img className='border-soild rounded border-2 w-[3%] h-6' src='https://t4.ftcdn.net/jpg/01/88/66/97/240_F_188669798_TzP74MjnqVivXdnJeVntMlF7fnASfHAy.jpg' ></img>98/kg)
+       
+         <div className=' text-lime-500 w-[2%] h-[3%] mr-40 border-soild rounded'>51%off</div></div></div></div>
+         <div className='flex'>
+          <button className='border-soild rounded bg-red-600 mr-3'>Add to card</button>
+
+         </div>
+        
+        <img className='w-[3%] h-[%] border-solid border-2 rounded  inline-block' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTr5FFCo7qmIjg8I8gBXK5OlSz6xYtglPIUXQ&usqp=CAU'></img>
+        
+        </div>
+      
+        
+        </div>
+        <div>
+        <h2>subscribe my channel</h2>
+        <h2>subscribe my channel</h2>
+        <h2>subscribe my channel</h2>
+        <h2>subscribe my channel</h2>
+        <h2>subscribe my channel</h2>
+        <h2>subscribe my channel</h2>
+        <h2>subscribe my channel</h2>
+        <h2>subscribe my channel</h2>
+        <h2>subscribe my channel</h2>
+        <h2>subscribe my channel</h2>
+        <h2>subscribe my channel</h2>
+        <h2>subscribe my channel</h2>
+        <h2>subscribe my channel</h2>
+        <h2>subscribe my channel</h2>
+        <h2>subscribe my channel</h2>
+        <h2>subscribe my channel</h2>
+        <h2>subscribe my channel</h2>
+        <h2>subscribe my channel</h2>
+        <h2>subscribe my channel</h2>
+        <h2>subscribe my channel</h2>
+        <h2>subscribe my channel</h2>
+        <h2>subscribe my channel</h2>
+        <h2>subscribe my channel</h2>
+        <h2>subscribe my channel</h2>
+        <h2>subscribe my channel</h2>
+        <h2>subscribe my channel</h2>
+        <h2>subscribe my channel</h2>
+        <h2>subscribe my channel</h2>
+        </div>
+    
+
+        
+        
+    
+
+      
+      
+  
+    </>
+  )
+}
+
+export default StickyBottomBar
---
+++ my-project/app/page.js
@@ -0,0 +1,16 @@
+import StickyBottomBar from './components/StickyBottomBar'
+
+
+   export default function Home() {
+  return (
+   <main>
+      <StickyBottomBar/>
+
+
+
+
+ 
+
+   </main>
+  );
+}
